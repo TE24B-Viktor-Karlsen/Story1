05795e33cba0c4c2bcdfb03f8276579cc45e57f1
TE24B-viktor-karlsen <viktor.karlsen@elev.ga.ntig.se>
2025-09-01 11:05:40 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Du har vaknat en vanlig skoldag. Ett virus har sträckt sig över hela världen. Ingen är hemma, vad gör du?");
+string name = Console.ReadLine();
+
+
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// bool isAlive = true;
+// isAlive = 12 > 10;
+// isAlive = 6 < 10;
+// isAlive = 6 != 10;
+// isAlive = 10 == 10;
+
+
+
+
+// string name = Console.ReadLine();
+
+// if (name == "Viktor")
+// {
+//     Console.WriteLine("Välkommen!");
+// }
+// else if (name == "Gustav")
+// {
+//     Console.WriteLine("Lite Välkommen!");
+// }
+// else
+// {
+//      Console.WriteLine("Du lucktar dachri!");  
+// }
+
+  
+
+
+// Console.ReadLine();
